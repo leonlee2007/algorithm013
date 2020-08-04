41feed8ee022c49587a1156fdf6fed42c49d8443
phenix <183385685@qq.com>
2020-08-04 22:27:41 +0800

    移动零新的解法

--- Week_01/move_zeroes_test.go
+++ Week_01/move_zeroes_test.go
@@ -10,13 +10,33 @@ func TestMoveZeroes(t * testing.T) {
   fmt.Println("============Start TestMoveZeroes=============")
   nums := []int {0,1,0,3,12}
   fmt.Println("input:", nums)
-  moveZeroes(nums)
+  moveZeroes1(nums)
+  // moveZeroes2(nums)
   fmt.Println("output:", nums)
   // if maxArea != 49 { panic("result_is_error") }
   fmt.Println("============End TestMoveZeroes=============")
 }
 
-func moveZeroes(nums []int)  {
+// 方法一
+// 时间复杂度:O(n)
+// 空间复杂度:O(1)
+func moveZeroes1(nums []int)  {
+    j := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            nums[j] = nums[i]
+            j ++
+        }
+    }
+    for ;j < len(nums); j ++ {
+        nums[j] = 0
+    }
+}
+
+// 方法二 最优解
+// 时间复杂度:O(n)
+// 空间复杂度:O(1)
+func moveZeroes2(nums []int)  {
     zeroPos := 0
     for i := 0; i < len(nums); i++ {
         if nums[i] != 0 {
