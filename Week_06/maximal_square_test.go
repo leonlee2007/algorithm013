6e7575fad214cc885e2d770b4c6d9a7415dac323
leon <leon@MacdeiMac.local>
2020-09-06 22:15:23 +0800

    最大正方形

--- Week_06/maximal_square_test.go
+++ Week_06/maximal_square_test.go
@@ -1,4 +1,17 @@
+// 221. 最大正方形
+// 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
 
+// 示例:
+
+// 输入:
+// {
+//     { '1','1','1','1','0'},
+//     { '1','1','0','1','0'},
+//     {'1','1','0','0','0'},
+//     { '0','0','0','0','0'},
+//   }
+
+// 输出: 4
 package week_06
 
 import(
@@ -24,32 +37,26 @@ func TestMaximalSquare(t * testing.T) {
 func maximalSquare(matrix [][]byte) int {
     if len(matrix) == 0{ return 0 }
     m, n, ret := len(matrix), len(matrix[0]), 0
-    dp := make([][]int,m+1)
-    for i:=0;i<m+1;i++{ dp[i] = make([]int,n+1) }
-    for i:=1;i<m+1;i++{
-        for j:=1;j<n+1;j++{
-            if matrix[i-1][j-1] == '1'{
-                dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+    dp := make([][]int, m+1)
+    for i:=0; i < m + 1; i++ {dp[i] = make([]int, n + 1) }
+    for i:=1;i<m+1;i++ {
+        for j:=1;j < n+1; j++ {
+            if matrix[i - 1][j - 1] == '1'{
+                dp[i][j] = min(min(dp[i - 1][j],dp[i][j - 1]),dp[i - 1][j - 1]) + 1
                 ret = max(ret, dp[i][j])
             }
         }
     }
     return ret * ret
 }
-func min(a,b,c int)int{
-    if a > b{
-        if b < c { return b }
-        return c
-    } else {
-        if a < c{ return a }
-        return c
-    }
+
+func min(a, b int)int{
+  if a < b {return a}
+  return b
 }
 
-func max(a, b int)int{
-    if a > b{
-        return a
-    }
+func max(a, b int) int{
+    if a > b { return a }
     return b
- }
+}
 
